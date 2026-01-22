d27f48d296783de4f87a511d0b8766ff46a469fe
zhaohao <hao@zhao.im>
2026-01-22 22:45:32 +0800

    一些调整

--- src/config.ts
+++ src/config.ts
@@ -16,7 +16,7 @@ export const SITE = {
     text: "Edit page",
     url: "https://github.com/zhaohao/howardzhao/edit/main/",
   },
-  dynamicOgImage: true,
+  dynamicOgImage: false,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
   timezone: "Asia/shanghai", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
